444b68b73b5fd772941bf1f5d07c1d77595358b8
ryannnnnwang <outtathewoods8@gmail.com>
2022-07-03 16:55:49 +0800

    新分支 dev

--- type-challenges/easy/11-Tuple-to-Object/template.ts
+++ type-challenges/easy/11-Tuple-to-Object/template.ts
@@ -1,3 +1,33 @@
 type TupleToObject<T extends readonly any[]> = {
 	[P in T[number]]: P
 }
+
+function ensureArray(input: number | number[]): number[] {
+	if (Array.isArray(input)) {
+		return input
+	} else {
+		return [input]
+	}
+}
+
+interface Foo {
+	kind: 'foo'
+	diffType: string
+	fooOnly: boolean
+	shared: number
+}
+
+interface Bar {
+	kind: 'bar'
+	diffType: number
+	barOnly: boolean
+	shared: number
+}
+
+function handle1(input: Foo | Bar) {
+	if (input.kind === 'foo') {
+		input.fooOnly
+	} else {
+		input.barOnly
+	}
+}
